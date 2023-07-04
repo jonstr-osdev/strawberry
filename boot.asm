829fbf2c3d86b6f28fe6f663dea6c407270a7537
JON STR <jonstr.osdev@gmail.com>
2023-07-03 23:26:43 -0400

    stopping-point commit: Need to fix pixel method (overflow)

--- boot.asm
+++ boot.asm
@@ -15,8 +15,8 @@ _start:
 
 	call _enable_cursor_16r
 
-	mov dl, 34							; row
-	mov dh, 12							; column
+	mov dl, 30							; row
+	mov dh, 10							; column
 	call _move_cursor_16r
 
 	mov ah, 0x0e						; BIOS print char 	
@@ -26,9 +26,9 @@ _start:
 	lodsb								; Move the character at the address in SI into AL and inc si
 	int 0x10							; BIOS print char
 
-	push ax
-	mov cx, 0x0007						
-	mov dx, 0xA120						; 0x0007 A120 is 500,000 microseconds								; preserve al
+	push ax                             ; preserve al
+	mov cx, 0x0001						
+	mov dx, 0xE848												
 	call _wait_16r
 	pop ax
 
@@ -36,18 +36,20 @@ _start:
 	jne .next_char
 
 	mov cx, 0x0007						
-	mov dx, 0xA120						; 0x0007 A120 is 500,000 microseconds
+	mov dx, 0xA120						
 	call _wait_16r
 
 	call _clear_screen_16r
 
-.init_prompt;
+.init_prompt:
 	mov al, '>'
 	call _print_char_16r
 
 .prompt:
 	call _read_char_16r
 	call _print_char_16r
+	cmp al, 'V'
+	je _vga_fill_test
 	cmp al, 'X'
 	jne .prompt
 
@@ -56,7 +58,24 @@ _start:
 
 	jmp $								; hang
 
-	v_string db 'STRAWBERRY OS 0.0',0		; Put our string in memory
+	v_string db 'STRAWBERRY OS 0.0',0	; Put our string in memory
+
+
+_vga_fill_test:
+	call _set_vga_mode_16r
+
+	mov ax, 0xA000
+	mov es, ax
+
+	xor bx, bx
+	mov al, 0xB
+.put_pixel:
+	mov byte [es:bx], al
+	inc bx
+	cmp bx, 0x19A00
+	jne .put_pixel
+	
+	jmp $
 
 	%include "inc/subs_16r.asm"
 
